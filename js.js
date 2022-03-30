eb4454da494a5a82acad766174cfccded9058a0d
KirHarty <70472768+KirHarty@users.noreply.github.com>
2022-03-30 17:18:38 +0300

    react

---
+++ js.js
@@ -0,0 +1,8 @@
+let entry = prompt ("Вы гость?")
+function entryPass() {
+    if (prompt=="гость") {
+        alert("Все Оки,проходите!=)");
+    }else{
+        alert("обратитесь куда нить");
+    }
+}
